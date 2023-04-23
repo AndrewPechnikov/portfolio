e1dcee2e5b210f4a2a55980d534b1096193388a8
AndrewPechnikov <andrij.pechnikov@gmail.com>
2023-04-23 19:16:20 +0300

    fix-bugs

--- src/localStorage.js
+++ src/localStorage.js
@@ -3,11 +3,9 @@ const { data } = require("autoprefixer");
 let countVisit = localStorage.getItem("count_visit") ? localStorage.getItem("count_visit") : 0;
 countVisit++;
 localStorage.setItem("count_visit", countVisit);
-console.log(countVisit);
+console.log(`Ви були на сайті ${countVisit} разів.`);
 
 let countTimeOnSite = Number(localStorage.getItem("countTimeOnSite") ? localStorage.getItem("countTimeOnSite") : 0);
-
-console.log(countTimeOnSite);
 
 console.log(
 	`Ви перебували на сайті ${Math.floor(countTimeOnSite / (1000 * 60 * 60))} годин ${
@@ -17,8 +15,6 @@ console.log(
 let time1 = new Date().getTime();
 let time2 = new Date().getTime();
 
-console.log(time1);
-console.log(time2);
 setInterval(() => {
 	time2 = new Date().getTime();
 	diffTime = time2 - time1;
